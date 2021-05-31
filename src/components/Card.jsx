6a0a9439ac7d942e140cce2eb150fefdfa8f9dda
FacundoLG <facu-judoca@hotmail.com.ar>
2021-05-31 09:55:41 -0300

    random quote page added, now i'am working in the list of quotes

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BsFillForwardFill} from 'react-icons/bs'
+import '../assets/components/Card.css'
+const Card = ({author,description}) =>{
+    return(
+        <div className="cardContainer">
+            <div>
+                <h3 className="author">{author}</h3>
+                <p className="desc">{description}</p>
+            </div>
+            <BsFillForwardFill className="arrow"/>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BsArrowRepeat } from "react-icons/bs";
 import Quote from '../components/Quote';
+import Card from '../components/Card';
 import '../assets/pages/Home.css'
+var randomData = {}
 const Home = () =>{
+    const [data,setData] = useState(randomData)
     
-    const [quote,setQuote] = useState("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat sequi esse minima iste possimus tenetur neque in eos vel odio nihil maiores architecto, doloribus eum. Repudiandae architecto ipsam commodi reprehenderit!")
+    useEffect(() => {
+        getRandomData()        
+    },[])
+    const getRandomData = () => {
+        fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            randomData = {}
+            const {quoteAuthor,quoteGenre,quoteText,_id} = json.data[0]
+            randomData.author = quoteAuthor
+            randomData.genre = quoteGenre
+            randomData.text = quoteText
+            randomData.id = _id
+            setData(randomData)
+        })
+    }
     return(
         <div>
             <div className="buttonContainer">
-                <button className="randomButton">random <BsArrowRepeat className="icon"/></button>
+                <button className="randomButton" onClick={getRandomData}>random <BsArrowRepeat className="icon"/></button>
+            </div>
+            <div className="container card">
+                <Quote text={data.text}/>
             </div>
             <div className="container">
-                <Quote text={quote}/>
-                <Quote text={quote}/>
+                <Card author={data.author} description={data.genre}/>
             </div>
         </div>
     )
